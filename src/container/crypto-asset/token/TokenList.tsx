f249ef7e445ccc7cbff4170f947d1f6bc776f8ef
sean <sean.snow@live.com>
2022-07-18 17:16:14 +0800

    feat: token list

---
+++ src/container/crypto-asset/token/TokenList.tsx
@@ -0,0 +1,26 @@
+import { FlatList } from 'native-base';
+import React from 'react';
+import { WalletToken } from '../../../entity/Wallet';
+import { TokenListItem } from './TokenListItem';
+import { IFlatListProps } from 'native-base/lib/typescript/components/basic/FlatList';
+
+export type TokenListProps = {
+  onSelect?: (token: WalletToken) => void;
+} & Omit<IFlatListProps<WalletToken>, 'renderItem'>;
+
+/**
+ * token 列表
+ * @param tokens
+ * @constructor
+ */
+export function TokenList({ onSelect, ...props }: TokenListProps) {
+  function handlePress(item: WalletToken) {
+    onSelect?.(item);
+  }
+
+  function renderItem({ item }: { item: WalletToken }) {
+    return <TokenListItem {...item} onPress={() => handlePress(item)} />;
+  }
+
+  return <FlatList {...props} renderItem={renderItem} />;
+}
