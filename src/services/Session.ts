753ae1324da6748e62ac7b319afd92845ddc0d1b
sean <sean.snow@live.com>
2022-08-02 20:52:23 +0800

    feat: 取消钱包自动创建钱包

--- src/services/Session.ts
+++ src/services/Session.ts
@@ -141,8 +141,6 @@ export class Session {
   private async handleIdentifyInit(password: string, mnemonic: string, mnemonicPassword?: string) {
     await this.login({ mnemonic, password: mnemonicPassword });
     console.log(`DID 身份登录成功`);
-    // 登录成功 初始化钱包
-    await walletService.initHDWallet(mnemonic, mnemonicPassword);
     await Keychain.setGenericPassword('oneverse', password);
     await this.initDevicePassword(password);
     await repository.saveMnemonic(mnemonic, mnemonicPassword);
